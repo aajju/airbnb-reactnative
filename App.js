00b303137fcc16e5857fc708623ad97ce8118ac2
aajju <aajju@naver.com>
2022-05-25 10:52:22 +0900

    setup completed

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
 import { Asset } from "expo-asset";
+import { Ionicons } from "@expo/vector-icons";
+import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
 
 const cacheImages = (images) =>
@@ -14,6 +16,8 @@ const cacheImages = (images) =>
     }
   });
 
+const cacheFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+
 export default function App() {
   const [isReady, setIsReady] = useState(false);
   const handleFinish = () => setIsReady(true);
@@ -22,7 +26,10 @@ export default function App() {
       require("./assets/loginBg.jpeg"),
       "http://logok.org/wp-content/uploads/2014/07/airbnb-logo-belo-219x286.png",
     ];
-    console.log(cacheImages(images));
+    const fonts = [Ionicons.font];
+    const imagePromises = cacheImages(images);
+    const fontPromises = cacheFonts(fonts);
+    return Promise.all([...fontPromises, ...imagePromises]);
   };
   return isReady ? (
     <Text>I'm ready</Text>
