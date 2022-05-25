03b33acc40c1f394dac0b08aafea8fadf71088eb
aajju <aajju@naver.com>
2022-05-25 10:41:17 +0900

    setup splash expo

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
+import { useState } from "react";
+
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Asset } from "expo-asset";
+import AppLoading from "expo-app-loading";
+
+const cacheImages = (images) =>
+  images.map((image) => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hi welcome!</Text>
-      <StatusBar style="auto" />
-    </View>
+  const [isReady, setIsReady] = useState(false);
+  const handleFinish = () => setIsReady(true);
+  const loadAssets = async () => {
+    const images = [
+      require("./assets/loginBg.jpeg"),
+      "http://logok.org/wp-content/uploads/2014/07/airbnb-logo-belo-219x286.png",
+    ];
+    console.log(cacheImages(images));
+  };
+  return isReady ? (
+    <Text>I'm ready</Text>
+  ) : (
+    <AppLoading
+      onError={console.error}
+      onFinish={handleFinish}
+      startAsync={loadAssets}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
